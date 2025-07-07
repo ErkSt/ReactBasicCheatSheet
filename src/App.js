479cbf001d6c7e382e4c8d149c6c33cc30543be7
ErkSt <erik_steg@yahoo.com>
2025-07-07 07:25:33 -0300

    First Commit to Init Repository

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+class HelloWorld extends Component{
+  render(){
+    return <h3>  Hello World. </h3>
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primera demo de React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
